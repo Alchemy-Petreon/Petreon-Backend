fc3575b34f649ed026fb863ea3a519a0ee4c2841
Sjaan H <sjaanahydrick@gmail.com>
2021-01-13 10:09:51 -0800

    Feature/writing sql databases (#3)
    * users table written
    
    * pets table written 5
    
    * reformatted users and pets tables
    
    * subscriptions table written 9
    
    * posts table written 10
    
    * adjusted users table
    
    * comments table written 14

---
+++ sql/setup.sql
@@ -0,0 +1,51 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS pets CASCADE;
+DROP TABLE IF EXISTS subscriptions CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
+DROP TABLE IF EXISTS comments;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    account_created TIMESTAMP NOT NULL,
+    profile_pic TEXT NOT NULL,
+    profile_description TEXT NOT NULL,
+    likes BIGINT
+);
+
+CREATE TABLE pets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT REFERENCES users(id),
+    pet_name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    account_created TIMESTAMP NOT NULL,
+    profile_pic TEXT NOT NULL,
+    profile_description TEXT NOT NULL,
+    banner_pic TEXT NOT NULL
+);
+
+CREATE TABLE subscriptions (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    pet_id BIGINT REFERENCES pets(id),
+    user_id BIGINT REFERENCES users(id)
+);
+
+CREATE TABLE posts (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    pet_id BIGINT REFERENCES pets(id),
+    post_time TIMESTAMP NOT NULL,
+    pic_url TEXT,
+    video_url TEXT,
+    post_text TEXT,
+    likes BIGINT
+);
+
+CREATE TABLE comments (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT REFERENCES users(id),
+    post_id BIGINT REFERENCES post(id),
+    text TEXT NOT NULL,
+    timestamp TIMESTAMP NOT NULL
+);
