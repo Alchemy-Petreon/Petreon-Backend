4b7fe69ccc826c61a879b4ff209fc78a958ca888
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-01-13 09:44:44 -0800

    pets table written 5

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,5 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS pets;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -13,3 +14,15 @@ CREATE TABLE users (
     access_token TEXT NOT NULL,
     likes BIGINT
 );
+
+CREATE TABLE IF EXISTS pets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    pet_name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    account_created TIMESTAMP NOT NULL,
+    profile_pic TEXT NOT NULL,
+    profile_description TEXT NOT NULL,
+    banner_pic TEXT NOT NULL,
+    posts BIGINT REFERENCES posts(id)
+);
+
