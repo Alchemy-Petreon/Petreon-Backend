c2b8f7ee12343f3d6c8fde7cbd46354e82454600
Paul Stevens <paul.stevens.developer@gmail.com>
2021-01-18 00:54:31 -0800

    adds seed data to user

---
+++ sql/seed-data/seed-data.js
@@ -0,0 +1,22 @@
+export const petArray = [{
+    petName: 'KeyboardCat',
+    type: 'cat',
+    profilePicture: 'http://placekitten.com/200/200',
+    profileDescription: 'where the wet food at?'
+}, {
+    petName: 'Maru',
+    type: 'also cat',
+    profilePicture: 'http://placekitten.com/250/250',
+    profileDescription: "I'm just here for the pussy"
+}]
+
+export const postArray = [{
+    pictureUrl: 'http://placekitten.com/100/100',
+    postText: '#blessed'
+}, {
+    pictureUrl: 'http://placekitten.com/300/300',
+    postText: '#nofilter'
+}, {
+    pictureUrl: 'https://place-puppy.com/150x150',
+    postText: 'Check this bitch'
+}]
