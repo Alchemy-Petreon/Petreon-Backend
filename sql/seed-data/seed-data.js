abaf2abd1762fe9331efaadf3c74bc88256504b3
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-01-18 17:32:24 -0800

    Feature/adds pet pictures (#47)
    * updated file.url
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * adds seed data to user
    
    * testing
    
    * adds /picture/ and /banner/ routes to pet
    
    * adds /picture/ and /banner/ routes to pet
    
    * adds /picture/ and /banner/ routes to pet
    
    * adds /picture/ and /banner/ routes to pet
    
    * adds /picture/ and /banner/ routes to pet
    
    * update photo on post
    
    * testing new post creation functionality
    
    * testing new post creation functionality
    
    * testing new post creation functionality
    
    * testing new post creation functionality
    
    * testing new post creation functionality
    
    * testing new post creation functionality
    
    * testing new post creation functionality
    
    * testing new post creation functionality
    
    * testing new post creation functionality
    
    * testing new post creation functionality
    
    * testing new post creation functionality
    
    * testing new post creation functionality
    
    * testing new post creation functionality
    
    * testing new post creation functionality
    
    * fixes all problems

---
+++ sql/seed-data/seed-data.js
@@ -0,0 +1,26 @@
+const petArray = [{
+    petName: 'KeyboardCat',
+    type: 'cat',
+    petProfilePicture: 'http://placekitten.com/200/200',
+    petProfileDescription: 'where the wet food at?',
+    bannerPicture: 'http://placekitten.com/800/200'
+}, {
+    petName: 'Maru',
+    type: 'cat',
+    petProfilePicture: 'http://placekitten.com/250/250',
+    petProfileDescription: "I'm just here for the pussy",
+    bannerPicture: 'http://placekitten.com/900/200'
+}]
+
+const postArray = [{
+    pictureUrl: 'http://placekitten.com/100/100',
+    postText: '#blessed'
+}, {
+    pictureUrl: 'http://placekitten.com/300/300',
+    postText: '#nofilter'
+}, {
+    pictureUrl: 'https://place-puppy.com/150x150',
+    postText: 'Check this bitch'
+}]
+
+module.exports = { petArray, postArray }
