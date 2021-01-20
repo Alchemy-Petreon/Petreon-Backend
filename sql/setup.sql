b0397cd243c7d8693bc8d6d9d6f1902663251395
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-01-20 12:09:26 -0800

    update comment date structure

--- sql/setup.sql
+++ sql/setup.sql
@@ -12,12 +12,13 @@ CREATE TABLE users (
     account_created TEXT NOT NULL,
     profile_picture TEXT,
     profile_description TEXT NOT NULL,
+    venmo TEXT,
     likes BIGINT
 );
 
 CREATE TABLE pets (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id BIGINT REFERENCES users(id),
+    user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
     pet_name TEXT NOT NULL,
     type TEXT NOT NULL,
     account_created TEXT NOT NULL,
@@ -28,14 +29,14 @@ CREATE TABLE pets (
 
 CREATE TABLE subscriptions (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    pet_id BIGINT REFERENCES pets(id),
-    user_id BIGINT REFERENCES users(id)
+    pet_id BIGINT REFERENCES pets(id) ON DELETE CASCADE,
+    user_id BIGINT REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE posts (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    pet_id BIGINT REFERENCES pets(id),
-    user_id BIGINT REFERENCES users(id),
+    pet_id BIGINT REFERENCES pets(id) ON DELETE CASCADE,
+    user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
     post_time TEXT NOT NULL,
     media_url TEXT,
     media_type TEXT,
@@ -45,8 +46,9 @@ CREATE TABLE posts (
 
 CREATE TABLE comments (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id BIGINT REFERENCES users(id),
-    post_id BIGINT REFERENCES posts(id),
+    user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    post_id BIGINT REFERENCES posts(id) ON DELETE CASCADE,
     text TEXT NOT NULL,
     timestamp TEXT NOT NULL
 );
+
